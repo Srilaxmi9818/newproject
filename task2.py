b9b8730acedb02c20af316e984b63dea262d5c70
Srilaxmi9818 <sreelakshmi9818@gmail.com>
2024-04-08 16:10:32 +0100

    commiting

--- task2.py
+++ task2.py
@@ -15,4 +15,28 @@ print ("Hello",user_name,"your favourite colour is",favourite_colour)
 #Ask the user for a number and print the square of that number
 number = int (input("Enter the number to be squared:"))
 result = number*number
-print (result)
+print ("The square of",number,"is",result)
+
+#Write a program that asks for 2 numbers and prints their sum, difference, product and quotient
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter the second number:"))
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+quotient = number1 / number2
+print ("Sum is",sum)
+print ("Difference is",difference)
+print ("Product is",product)
+print ("Quotient is",quotient)
+
+#Compare 2 numbers and print which is larger or if they are equal
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+if (num1>num2):
+    print ("Larger number is",num1)
+elif (num1<num2):
+    print ("Larger number is",num2)
+else:
+    print ("They are equal")
+    
+
